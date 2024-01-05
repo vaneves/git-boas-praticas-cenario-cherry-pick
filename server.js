0c708103f685620c82cea9abed50beeeaa5e901c
Van Neves <vaneves@vaneves.com>
2024-01-05 10:53:46 -0300

    feat/add-school cria configuração da porta

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
